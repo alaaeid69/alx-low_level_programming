2075c5aafabe8de99a7bf362313d10df20337bc7
alaaeid69 <alaaeid69.com>
2023-03-17 19:57:37 -0700

    u

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+int a;
+for (a = 0; a <= 10; a++)
+putchar(a);
+putchar('\n');
+return (0);
+
+}
