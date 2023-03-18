6f6fb0b8021b9390e7b257c0a3c3a473f4ad9c73
alaaeid69 <alaaeid69.com>
2023-03-17 19:53:42 -0700

    u

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+if ((c != 'q' && c != 'e') && c <= 'z')
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+
+}
