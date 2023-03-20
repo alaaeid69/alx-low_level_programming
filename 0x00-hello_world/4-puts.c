edb3befcdb08a42ebe661dc9a725e49cee4389f2
alaaeid69 <alaaeid69.com>
2023-03-20 16:40:00 -0700

    op

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line"
- * return :Always 0.
+ * main - prints "Programming is like building a multilingual puzzle, followed by a new l
+ * * Return: Always 0 (success)
  */
 int main(void)
 {
