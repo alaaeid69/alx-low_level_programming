d6c68d362c6178861513144808c21303889d2b7e
alaaeid69 <alaaeid69.com>
2023-03-20 16:50:26 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - printf("with proper grammar, but the outcome is a piece of art")
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
+
